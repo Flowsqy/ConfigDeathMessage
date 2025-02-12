42886b509e81b6dc0afae4a6610c4b9c626fc1bf
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-12 16:43:29 +0100

    Load message registry at startup

--- plugin/src/main/java/fr/flowsqy/configdeathmessage/ConfigDeathMessagePlugin.java
+++ plugin/src/main/java/fr/flowsqy/configdeathmessage/ConfigDeathMessagePlugin.java
@@ -1,12 +1,25 @@
 package fr.flowsqy.configdeathmessage;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.flowsqy.configdeathmessage.config.ConfigLoader;
+import fr.flowsqy.configdeathmessage.config.MessageRegistryLoader;
 
 public class ConfigDeathMessagePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        final var configLoader = new ConfigLoader();
+        final var dataFolder = getDataFolder();
+        final var logger = getLogger();
+        if (!configLoader.checkDataFolder(dataFolder)) {
+            Bukkit.getPluginManager().disablePlugin(this);
+            logger.warning("Can't write in the plugin directory. Disable the plugin");
+            return;
+        }
+        final var messageRegistryLoader = new MessageRegistryLoader();
+        messageRegistryLoader.load(configLoader, this, "messages.lang");
     }
 
 }
