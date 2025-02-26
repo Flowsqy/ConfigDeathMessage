e6e018932703c668e6a1ceda880379eecf61f436
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-26 15:28:16 +0100

    Implement component replacer

--- plugin/src/main/java/fr/flowsqy/configdeathmessage/message/ComponentReplacer.java
+++ plugin/src/main/java/fr/flowsqy/configdeathmessage/message/ComponentReplacer.java
@@ -1,6 +1,9 @@
 package fr.flowsqy.configdeathmessage.message;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import org.jetbrains.annotations.NotNull;
@@ -11,38 +14,55 @@ import net.md_5.bungee.api.chat.TextComponent;
 public class ComponentReplacer {
 
     @NotNull
-    public BaseComponent replace(@NotNull BaseComponent message, @NotNull String regex,
+    public void replace(@NotNull BaseComponent message, @NotNull String regex,
             @NotNull BaseComponent replacement) {
-        return replace(message, Pattern.compile(regex), replacement);
+        replace(message, Pattern.compile(regex), replacement);
     }
 
     @NotNull
-    public BaseComponent replace(@NotNull BaseComponent message, @NotNull Pattern pattern,
+    public void replace(@NotNull BaseComponent message, @NotNull Pattern pattern,
             @NotNull BaseComponent replacement) {
-        final var newComponent = new TextComponent();
-        newComponent.copyFormatting(message);
-        if (message instanceof TextComponent textComponent) {
-            final var matcher = pattern.matcher(textComponent.getText());
-            // TODO Build a new TextComponent with extra named after
-            /*
-             * boolean result = matcher.find();
-             * if (result) {
-             * do {
-             * matcher.start();
-             * appendReplacement(sb, replacement);
-             * result = find();
-             * } while (result);
-             * appendTail(sb);
-             * return sb.toString();
-             * }
-             * return text.toString();
-             */
+        final var originalToExtras = originalToExtras(message, pattern, replacement);
+        final var previousExtras = message.getExtra();
+        final var extras = new ArrayList<BaseComponent>(
+                originalToExtras.size() + (previousExtras == null ? 0 : previousExtras.size()));
+        extras.addAll(originalToExtras);
+        if (previousExtras != null) {
+            for (var extra : previousExtras) {
+                replace(extra, pattern, replacement);
+            }
+            extras.addAll(previousExtras);
         }
-        final var extras = new ArrayList<BaseComponent>(message.getExtra().size());
-        for (var extra : message.getExtra()) {
-            extras.add(replace(extra, pattern, replacement));
-        }
-        newComponent.setExtra(extras);
-        return newComponent;
+        message.setExtra(extras);
     }
+
+    @NotNull
+    private List<BaseComponent> originalToExtras(@NotNull BaseComponent message, @NotNull Pattern pattern,
+            @NotNull BaseComponent replacement) {
+        if (!(message instanceof TextComponent textComponent)) {
+            return Collections.emptyList();
+        }
+        final var text = textComponent.getText();
+        final var matcher = pattern.matcher(text);
+        if (!matcher.find()) {
+            return Collections.emptyList();
+        }
+        textComponent.setText("");
+        final var extras = new LinkedList<BaseComponent>();
+        int cursor = 0;
+        do {
+            final var betweenString = text.substring(cursor, matcher.start());
+            cursor = matcher.end();
+            if (!betweenString.isEmpty()) {
+                extras.add(new TextComponent(betweenString));
+            }
+            extras.add(replacement);
+        } while (matcher.find());
+        final var tailString = text.substring(cursor);
+        if (!tailString.isEmpty()) {
+            extras.add(new TextComponent(tailString));
+        }
+        return extras;
+    }
+
 }
