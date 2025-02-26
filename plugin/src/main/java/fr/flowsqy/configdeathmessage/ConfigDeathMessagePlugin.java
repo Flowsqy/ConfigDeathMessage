51dde03928da8b9be33de8ee06ffa1cc5c0b4d08
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-26 20:11:00 +0100

    Link the message registry loader to message manager

--- plugin/src/main/java/fr/flowsqy/configdeathmessage/ConfigDeathMessagePlugin.java
+++ plugin/src/main/java/fr/flowsqy/configdeathmessage/ConfigDeathMessagePlugin.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.flowsqy.configdeathmessage.config.ConfigLoader;
 import fr.flowsqy.configdeathmessage.config.MessageRegistryLoader;
+import fr.flowsqy.configdeathmessage.config.MessagesRegistrySanitizer;
 import fr.flowsqy.configdeathmessage.listener.PlayerDeathListener;
 import fr.flowsqy.configdeathmessage.message.MessagesManager;
 
@@ -22,7 +23,10 @@ public class ConfigDeathMessagePlugin extends JavaPlugin {
         }
         final var messageRegistryLoader = new MessageRegistryLoader();
         messageRegistryLoader.load(configLoader, this, "messages.lang");
-        final var messagesManager = new MessagesManager();
+        final var messageRegistry = messageRegistryLoader.loadMessages(logger);
+        final var messageRegistrySanitizer = new MessagesRegistrySanitizer();
+        final var sanitizedMessageRegistry = messageRegistrySanitizer.sanitize(logger, messageRegistry);
+        final var messagesManager = new MessagesManager(sanitizedMessageRegistry);
         Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(messagesManager), this);
     }
 
